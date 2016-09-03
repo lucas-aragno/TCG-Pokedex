6c74588fa28daa55e984157e86030acb81aee4da
lucas <lucas.aragno157@gmail.com>
2016-09-03 13:24:09 -0300

    wut

--- src/server.js
+++ src/server.js
@@ -3,15 +3,30 @@ import { Server } from 'http'
 import Express from 'express'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
+import webpack from 'webpack'
 
+import config from '../webpack.config'
 import Pokedex from './components/Pokedex'
+
+delete process.env.BROWSER
+
+const compiler = webpack(config)
 
 const app = new Express()
 const server = new Server(app)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(Express.static(path.join(__dirname, 'static')))
+// Use this when its production and you are
+// actually using static stuff
+// app.use(Express.static(path.join(__dirname, 'static')))
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: false,
+  publicPath: config.output.publicPath
+}))
+
+app.use(require('webpack-hot-middleware')(compiler))
 
 app.get('*', (req, res) => {
   let markup = renderToString(<Pokedex />)
--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,37 @@ const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'app.js'),
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, 'src', 'app.js')
+  ],
   output: {
+    filename: 'bundle.js',
     path: path.join(__dirname, 'src', 'static', 'js'),
-    filename: 'bundle.js'
+    publicPath: '/static/'
   },
   module: {
     loaders: [{
-      test: path.join(__dirname, 'src'),
-      loader: ['babel-loader'],
-      query: {
-        cacheDirectory: 'babel_cache'
-      }
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/,
+      include: path.join(__dirname, 'src')
+    },
+    {
+      loaders: ['style', 'raw'],
+      include: __dirname,
+      test: /\.css$/
     }]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        BROWSER: JSON.stringify(true)
+      }
     }),
     new webpack.optimize.DedupePlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
